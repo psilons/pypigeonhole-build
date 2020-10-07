5ba3f3adc62c72b56994f70f310ed901a502cf58
psilons <cuirick@gmail.com>
2020-10-07 18:07:03 -0400

    fix requirements.txt output path

--- dep_setup.py
+++ dep_setup.py
@@ -23,15 +23,16 @@ test_required = pip_dep_utils.get_test_required(dependent_libs)
 
 python_requires = pip_dep_utils.get_python_requires(dependent_libs)
 
+# we can't abstract this out since it knows pip and conda, maybe more later on.
 if __name__ == "__main__":
     if len(sys.argv) < 2:
         raise ValueError('need to pass in parameters: pip, conda, conda_env')
 
     if sys.argv[1] == 'pip':
-        pip_dep_utils.gen_req_txt(dependent_libs, 'test/test_pypigeonhole_build/requirements.txt')
+        pip_dep_utils.gen_req_txt(dependent_libs, './requirements.txt')
     elif sys.argv[1] == 'conda':
         conda_dep_utils.gen_conda_yaml(dependent_libs, './environment.yaml')
     elif sys.argv[1] == 'conda_env':
         print(CONDA.env)
     else:
-        raise ValueError(f'unkown parameter {sys.argv[1]}')
+        raise ValueError(f'unknown parameter {sys.argv[1]}')
