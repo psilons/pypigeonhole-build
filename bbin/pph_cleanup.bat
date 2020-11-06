7c002a0306b92710bac8b91b4876b701ce58a519
psilons <cuirick@gmail.com>
2020-11-06 16:58:20 -0500

    try one more time to delete folders. Maybe readonly causes the problem

--- bbin/pph_cleanup.bat
+++ bbin/pph_cleanup.bat
@@ -6,22 +6,22 @@ IF NOT EXIST setup.py (
 )
 echo Cleanup Project Folder: %ProjDir%
 
-if exist %ProjDir%\build RMDIR /Q /S %ProjDir%\build
+if exist %ProjDir%\build RMDIR /S /Q %ProjDir%\build
 if exist %ProjDir%\build EXIT /B 1
 
-if exist %ProjDir%\dist RMDIR /Q /S %ProjDir%\dist
+if exist %ProjDir%\dist RMDIR /S /Q %ProjDir%\dist
 if exist %ProjDir%\dist EXIT /B 1
 
-if exist %ProjDir%\dist_conda RMDIR /Q /S %ProjDir%\dist_conda
+if exist %ProjDir%\dist_conda RMDIR /S /Q %ProjDir%\dist_conda
 if exist %ProjDir%\dist_conda EXIT /B 1
 
-if exist %ProjDir%\dist_zip RMDIR /Q /S %ProjDir%\dist_zip
+if exist %ProjDir%\dist_zip RMDIR /S /Q %ProjDir%\dist_zip
 if exist %ProjDir%\dist_zip EXIT /B 1
 
-FOR /d %%G IN ("%ProjDir%\*.egg-info") DO RMDIR /Q /S "%%~G"
+FOR /d %%G IN ("%ProjDir%\*.egg-info") DO RMDIR /S /Q "%%~G"
 
 REM come from pip install -e .
-FOR /d %%G IN ("%ProjDir%\src\*.egg-info") DO RMDIR /Q /S "%%~G"
+FOR /d %%G IN ("%ProjDir%\src\*.egg-info") DO RMDIR /S /Q "%%~G"
 
 REM if we miss these 2, that's fine.
 echo Done
